2a872039dba68a6e39947a7ffb98f0136e267ad3
Christian Herzig <github@herzig.info>
2020-05-07 08:24:39 +0200

    game-loop: limit frame rate to 60 fps
    Signed-off-by: Christian Herzig <github@herzig.info>

--- main.cpp
+++ main.cpp
@@ -3,13 +3,25 @@
 
 int main()
 {
+  constexpr uint32_t FPS {60};
+  constexpr uint32_t frameDelay {1000/FPS};
+
   Game *game = new Game();
   game->init("sdl2game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
+
   while (game->running()) {
+    uint32_t frameStart = SDL_GetTicks();
+
     game->handleEvents();
     game->update();
     game->render();
+
+    // limit frame rate:
+    int32_t frameTime = SDL_GetTicks() - frameStart;
+    if (frameTime < frameDelay) {
+      SDL_Delay(frameDelay - frameTime);
+    }
   }
 
   game->clean();
