284d027d6049019f5914c51d8bb46560f7acecb3
Christian Herzig <github@herzig.info>
2020-05-11 15:31:52 +0200

    texture_manager: report image loading failure
    Signed-off-by: Christian Herzig <github@herzig.info>

--- src/texture_manager.cpp
+++ src/texture_manager.cpp
@@ -1,9 +1,13 @@
 #include "texture_manager.h"
 #include <SDL2/SDL_image.h>
+#include <iostream>
 
 SDL_Texture* TextureManager::LoadTexture(const char* texture) {
   SDL_Surface *tmpSurface = IMG_Load(texture);
   SDL_Texture *tex = SDL_CreateTextureFromSurface(Game::s_renderer, tmpSurface);
+  if (tex == nullptr) {
+    std::cout << "Error: Can't load Texture " << texture << "!" << std::endl;
+  }
   SDL_FreeSurface(tmpSurface);
 
   return tex;
